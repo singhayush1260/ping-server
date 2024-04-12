5e34bde07f27dabdbd81e2aeea9251d43319deaa
Ayush Singh <singh.dev6537@gmail.com>
2024-04-12 23:36:51 +0530

    added sameSite to cookie

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -35,6 +35,7 @@ export const signup = async (req: Request, res: Response) => {
 
     res.cookie("auth_token", token, {
       httpOnly: true,
+      sameSite:"none",
       secure: process.env.NODE_ENV === "production",
       maxAge: 86400000,
     });
@@ -80,6 +81,7 @@ export const login = async (req: Request, res: Response) => {
     );
     res.cookie("auth_token", token, {
       httpOnly: true,
+      sameSite:"none",
       secure: process.env.NODE_ENV === "production",
       maxAge: 86400000,
     });
@@ -117,6 +119,7 @@ export const changePassword = async (req:Request, res:Response) => {
 
 export const logout=(req:Request, res:Response)=>{
     res.cookie("auth_token","",{
+      sameSite:"none",
       expires:new Date(0)
     });
     res.send();
--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -10,19 +10,18 @@ declare global {
 }
 
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-  console.log("inside verify token")
+  console.log("inside verify token");
   const token = req.cookies["auth_token"];
   if (!token) {
-    console.log("no token")
+    console.log("no token");
     return res.status(401).json({ message: "unauthorized" });
   }
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
     req.userId = (decoded as JwtPayload).userId;
     next();
   } catch (error) {
-    console.log("inside auth middleware error")
+    console.log("inside auth middleware error");
     return res.status(401).json({ message: "unauthorized" });
   }
 };
